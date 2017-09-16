2af282c611e3880972b1f46e39ec83f2ef60f065
Jorge T <jotolo7@gmail.com>
2017-09-16 12:20:16 -0400

    initializing route model

--- models/route.rb
+++ models/route.rb
@@ -1,5 +1,12 @@
 class Route
-  def initialize
-
+  attr_accessor :passphrase, :source, :start_node, :end_node, :start_time, :end_time
+  def initialize(passphrase, source, start_node, end_node, start_time, end_time)
+    @passphrase = passphrase
+    @source = source
+    @start_node = start_node
+    @end_node = end_node
+    @start_time = start_time
+    @end_time = end_time
   end
+
 end
